5cf04e6eaf25d1c6342c2bfa7379a8305a858ea9
Olivia isberg <oliviaisberg@gmail.com>
2021-11-23 12:18:51 +0100

    Changed name on header to aside

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 window.addEventListener('load', main);
 
+/**Start of the program */
 function main() {
     addEventListeners();
 }
 
+/**Function that add onclick event on buttons */
 function addEventListeners() {
-    const hiddenMenuIcon = document.getElementById('hidden-menu-icon');
-    const header = document.querySelector('header');
-    hiddenMenuIcon.addEventListener('click', function(){
-        header.classList.toggle('show-menu');
-    })
+    let hiddenMenuIcon = document.getElementById('hidden-menu-icon');
+    hiddenMenuIcon.onclick = hideMenuOnClick;
+    
 }
+/**Function that hides sidemenu */
+function hideMenuOnClick() {
+    let aside = document.querySelector('aside');
+    aside.classList.toggle('show-menu');
+}
+
