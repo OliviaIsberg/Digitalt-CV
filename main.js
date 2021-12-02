c3361291df0764be8f10cfab78f51c6ebbdedf35
Olivia isberg <oliviaisberg@gmail.com>
2021-12-02 22:09:46 +0100

    Added contact form

--- main.js
+++ main.js
@@ -7,13 +7,23 @@ function main() {
 
 /**Function that add onclick event on buttons */
 function addEventListeners() {
-    let hiddenMenuIcon = document.getElementById('hidden-menu-icon');
-    hiddenMenuIcon.onclick = hideMenuOnClick;
-    
+    let menuToggle = document.getElementById('menu-toggle');
+    menuToggle.onclick = toggleMenu;
+
+    let contact = document.getElementById('contact');
+	contact.onclick = toggleContactForm;
 }
 /**Function that hides sidemenu */
-function hideMenuOnClick() {
+function toggleMenu(e) {
     let aside = document.querySelector('aside');
     aside.classList.toggle('show-menu');
+    aside.classList.remove('show-contact');
+	e.preventDefault();
 }
 
+function toggleContactForm(e) {
+	let aside = document.querySelector('aside');
+    aside.classList.toggle('show-contact');
+	aside.classList.remove('show-menu');
+	e.preventDefault();
+}
