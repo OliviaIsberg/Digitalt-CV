148b2bd32a7519127ae47aa54fdd3271455c857d
Olivia isberg <oliviaisberg@gmail.com>
2021-12-02 11:57:02 +0100

    Revert "Improved responsivity in line animation and fixed iOS stroke animation bug"
    This reverts commit 884bb9f45d3b38de08f5ea21781ea33e2b7bb398.

--- lineanim.js
+++ lineanim.js
@@ -18,26 +18,24 @@ function drawLine() {
 	let cLocator = document.getElementById('c-locator');
 	let photo = document.getElementById('photo');
 
-	let startingPointX = Math.max(width, 1500);
 	let cOffsetRight = cLocator.offsetLeft + cLocator.offsetWidth;
 	let cOffsetMidY = cLocator.offsetTop + cLocator.offsetHeight / 2;
-	let bezierHandle1X = cOffsetRight + (startingPointX - cOffsetRight) / 2
-	let cvRadius = parseInt(window.getComputedStyle(cLocator).getPropertyValue('font-size').slice(0, -2));
 	let midPointX = cOffsetRight - (photo.offsetLeft + photo.offsetWidth / 2)
 	let photoRadius = (photo.offsetWidth + parseInt(window.getComputedStyle(photo).getPropertyValue('outline-width').slice(0, -2))) / 2;
 	let photoMidX = photo.offsetLeft + photo.offsetWidth / 2;
 	let photoMidY = photo.offsetTop + photo.offsetHeight / 2
 	let photoBottomLeftX = photoMidX + (photoRadius * Math.cos(135 * Math.PI / 180));
 	let photoBottomLeftY = photoMidY + (photoRadius * Math.sin(135 * Math.PI / 180));
-	let bezierHandle2X = photoBottomLeftX + (photoRadius * 1.5 * Math.cos(45 * Math.PI / 180));
-	let bezierHandle2Y = photoBottomLeftY + (photoRadius * 1.5 * Math.sin(45 * Math.PI / 180));
+	let bezierHandleX = photoBottomLeftX + (photoRadius * 1.5 * Math.cos(45 * Math.PI / 180));
+	let bezierHandleY = photoBottomLeftY + (photoRadius * 1.5 * Math.sin(45 * Math.PI / 180));
 
 	let path = [
-		svgPathMove(startingPointX, height * 0.95),
-		svgPathCurve(bezierHandle1X, height * 0.95, bezierHandle1X, cOffsetMidY + cvRadius, cOffsetRight, cOffsetMidY + cvRadius),
-		svgPathArc(1, 1, 0, 0, 1, cOffsetRight, cOffsetMidY - cvRadius),
-		svgPathArc(1, 1, 0, 0, 1, cOffsetRight, cOffsetMidY + cvRadius),
-		svgPathCurve(midPointX, cOffsetMidY + cvRadius, bezierHandle2X, bezierHandle2Y, photoBottomLeftX, photoBottomLeftY),
+		svgPathMove(width * 1.05, height * 0.95),
+		svgPathCurve(width * 0.90, height * 0.95, width * 0.85, height * 0.76, width * 0.80, height * 0.58),
+		svgPathCurve(width * 0.75, height * 0.40, width * 0.70, cOffsetMidY + 80, cOffsetRight, cOffsetMidY + 80),
+		svgPathArc(1, 1, 0, 0, 1, cOffsetRight, cOffsetMidY - 80),
+		svgPathArc(1, 1, 0, 0, 1, cOffsetRight, cOffsetMidY + 80),
+		svgPathCurve(midPointX, cOffsetMidY + 80, bezierHandleX, bezierHandleY, photoBottomLeftX, photoBottomLeftY),
 		svgPathArc(photoRadius, photoRadius, 0, 0, 1, photoMidX, photoMidY - photoRadius),
 		svgPathArc(photoRadius, photoRadius, 0, 0, 1, photoMidX, photoMidY + photoRadius),
 		svgPathCurve(photoMidX - photoRadius * 2, photoMidY + photoRadius, width * -0.05, height * -0.15 + (photoMidY + photoRadius - height * -0.15) / 2, width * -0.05, height * -0.15),
@@ -45,4 +43,8 @@ function drawLine() {
 
 	pathElement.setAttribute('d', path);
 	pathElement.style.setProperty('--dashlen', pathElement.getTotalLength());
+	pathElement.style.setProperty('--dashoffsetafter', pathElement.getTotalLength() * -1);
 }
+
+window.addEventListener('load', drawLine);
+window.addEventListener('resize', drawLine);
